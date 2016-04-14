7746ae320f568ee3514110cb80cfbf47618dffa6
Collin <candrew022@gmail.com>
2016-04-14 14:23:13 -0400

    Create new root level container

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+
+import * as reducers from '../reducers';
+import GamePad from './GamePad';
+
+const reducer = combineReducers(reducers)
+const store = createStore(reducer);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <GamePad />
+      </Provider>
+    )
+  }
+}
+
+export default App;
